09b30846f9a9768999ed5d49bf61a49883dc7e88
Mislav Mandarić <2945713+MislavMandaric@users.noreply.github.com>
2021-09-09 17:57:57 +0200

    chore: Removes authlib 0.15.4 hack with default client

--- src/vaillant_netatmo_api/base.py
+++ src/vaillant_netatmo_api/base.py
@@ -4,10 +4,6 @@ from __future__ import annotations
 
 from typing import Awaitable, Callable
 
-### Start httpx==0.18.2 fix: https://github.com/natekspencer/pylitterbot/pull/5 ###
-import authlib.integrations.httpx_client.oauth2_client
-import httpx
-### End httpx==0.18.2 fix ###
 from authlib.integrations.httpx_client import AsyncOAuth2Client
 from authlib.oauth2.rfc6749 import OAuth2Token
 
@@ -16,19 +12,6 @@ _TOKEN_PATH = "/oauth2/token"
 _TOKEN_AUTH_METHOD = "client_secret_post"
 _TOKEN_PLACEMENT = "body"
 
-
-### Start httpx==0.18.2 fix: https://github.com/natekspencer/pylitterbot/pull/5 ###
-# httpx==0.18.2 switched to an object called `USE_CLIENT_DEFAULT` instead of `UNSET`
-# The latest release of Authlib (0.15.4 as of this writing) has not included this fix
-# though it does exist in the master branch of Authlib. Because of this, we need to
-# check the version and patch the issue if httpx is on or after version 0.18.2
-if httpx.__version__ >= "0.18.2" and not hasattr(
-    authlib.integrations.httpx_client.oauth2_client, "USE_CLIENT_DEFAULT"
-):
-    from httpx import USE_CLIENT_DEFAULT
-
-    authlib.integrations.httpx_client.oauth2_client.UNSET = USE_CLIENT_DEFAULT
-### End httpx==0.18.2 fix ###
 
 class BaseClient:
     """
